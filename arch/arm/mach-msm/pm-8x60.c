2ab475b4f5d077fd35d6423be2891a150e7baece
Matt Wagantall <mattw@codeaurora.org>
2011-04-13 15:17:11 -0700

    msm: pm-8x60: Remove redundant code for restoring ACPU clock rate
    The acpuclock driver takes care of saving and restoring the CPU
    frequency when hotplug removing and re-inserting a CPU.
    
    Remove similar functionality for the pm-8x60 driver.  The CPU
    frequency should have already been restored by acpuclock by the
    time msm_pm_platform_secondary_init() executes.
    
    (cherry picked from commit ef4f235b2e728e46fc03d2253c325d22987c1554)
    
    Change-Id: Iac5e7fd5bb8177fd1fa8527747baa1e7f1a5c5ec
    Signed-off-by: Raj Kushwaha <rajk@codeaurora.org>

--- arch/arm/mach-msm/pm-8x60.c
+++ arch/arm/mach-msm/pm-8x60.c
@@ -617,7 +617,6 @@ EXPORT_SYMBOL(msm_pm_set_max_sleep_time);
 struct msm_pm_device {
 	unsigned int cpu;
 #ifdef CONFIG_HOTPLUG_CPU
-	unsigned long saved_acpu_rate;
 	struct completion cpu_killed;
 	unsigned int warm_boot;
 #endif
@@ -1256,12 +1255,8 @@ void platform_cpu_die(unsigned int cpu)
 	flush_cache_all();
 
 	for (;;) {
-		if (allow[MSM_PM_SLEEP_MODE_POWER_COLLAPSE]) {
-			struct msm_pm_device *dev =
-				&__get_cpu_var(msm_pm_devices);
-			dev->saved_acpu_rate = acpuclk_get_rate(cpu);
+		if (allow[MSM_PM_SLEEP_MODE_POWER_COLLAPSE])
 			msm_pm_power_collapse(false);
-		}
 		else if (allow[MSM_PM_SLEEP_MODE_POWER_COLLAPSE_STANDALONE])
 			msm_pm_power_collapse_standalone(false);
 		else if (allow[MSM_PM_SLEEP_MODE_WAIT_FOR_INTERRUPT])
@@ -1289,16 +1284,6 @@ int msm_pm_platform_secondary_init(unsigned int cpu)
 #ifdef CONFIG_VFP
 	vfp_reinit();
 #endif
-
-	if (dev->saved_acpu_rate) {
-		ret = acpuclk_set_rate(dev->cpu,
-				dev->saved_acpu_rate,
-				SETRATE_PC);
-		if (ret)
-			pr_err("CPU%u: %s: failed clock rate restore(%lu)\n",
-			dev->cpu, __func__, dev->saved_acpu_rate);
-		dev->saved_acpu_rate = 0;
-	}
 	ret = msm_spm_set_low_power_mode(MSM_SPM_MODE_CLOCK_GATING, false);
 
 	return ret;
