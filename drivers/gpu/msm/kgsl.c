86d42049d0f0e6edf27e030cbd0c1ab52306b6e3
Jordan Crouse <jcrouse@codeaurora.org>
2011-04-28 10:31:41 -0600

    msm: kgsl: Reorganize debugfs code
    Reorganize debugfs functions, move them to new files better reflecting
    the new device split arrangement, consolidate initialization functions
    and make the files conditional on CONFIG_DEBUG_FS removing numerous
    
    Change-Id: I17932bd81fcede21e20df90825bb8ba52bc6eb1e
    Signed-off-by: Jordan Crouse <jcrouse@codeaurora.org>

--- drivers/gpu/msm/kgsl.c
+++ drivers/gpu/msm/kgsl.c
@@ -19,7 +19,6 @@
 #include <linux/fb.h>
 #include <linux/file.h>
 #include <linux/fs.h>
-#include <linux/debugfs.h>
 #include <linux/uaccess.h>
 #include <linux/init.h>
 #include <linux/list.h>
@@ -42,7 +41,7 @@
 #include "kgsl_g12.h"
 #include "kgsl_cmdstream.h"
 #include "kgsl_postmortem.h"
-
+#include "kgsl_debugfs.h"
 #include "kgsl_log.h"
 #include "kgsl_drm.h"
 #include "kgsl_cffdump.h"
@@ -1942,13 +1941,8 @@ kgsl_register_device(struct kgsl_device *device)
 
 	/* sysfs and debugfs initalization - failure here is non fatal */
 
-	/* Create a driver entry in the kgsl debugfs directory */
-	if (kgsl_debugfs_dir && !IS_ERR(kgsl_debugfs_dir))
-		device->d_debugfs = debugfs_create_dir(device->name,
-						       kgsl_debugfs_dir);
-
 	/* Initialize logging */
-	kgsl_device_log_init(device);
+	kgsl_device_debugfs_init(device);
 
 	/* Initialize common sysfs entries */
 	kgsl_pwrctrl_init_sysfs(device);
@@ -2146,7 +2140,7 @@ static int __init kgsl_core_init(void)
 		kobject_create_and_add("proc",
 				       &kgsl_driver.virtdev.kobj);
 
-	kgsl_debugfs_dir = debugfs_create_dir("kgsl", 0);
+	kgsl_core_debugfs_init();
 
 	kgsl_sharedmem_init_sysfs();
 	kgsl_cffdump_init();
