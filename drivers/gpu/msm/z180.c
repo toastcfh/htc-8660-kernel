7d76a50c8f285544ef864c0fb23d4cc39f27e503
Kevin DuBois <kdubois@codeaurora.org>
2011-05-10 11:52:12 -0700

    msm: kgsl: fix mmu fault arising from multicontext scenario
    under certain context destruction & creation scenarios, the
    mmu was not being reprogrammed appropriately with the new
    context's pagetable. This resulted in the core executing
    commands with pagetable not matching the context. Code resets
    the state of the mmu struct to default pagetable upon context
    destruction.
    
    Change-Id: If9f0f307b854158c90cfb7d3384e591c6575acf9
    CRs-fixed: 287344
    Signed-off-by: Kevin DuBois <kdubois@codeaurora.org>

--- drivers/gpu/msm/z180.c
+++ drivers/gpu/msm/z180.c
@@ -972,8 +972,11 @@ kgsl_g12_drawctxt_destroy(struct kgsl_device *device,
 
 	kgsl_g12_idle(device, KGSL_TIMEOUT_DEFAULT);
 
-	if (g12_device->ringbuffer.prevctx == context->id)
+	if (g12_device->ringbuffer.prevctx == context->id) {
 		g12_device->ringbuffer.prevctx = KGSL_G12_INVALID_CONTEXT;
+		device->mmu.hwpagetable = device->mmu.defaultpagetable;
+		kgsl_setstate(device, KGSL_MMUFLAGS_PTUPDATE);
+	}
 
 	return 0;
 }
