76331ee736e71f63c27594583cf527a5c512ff5e
Jordan Crouse <jcrouse@codeaurora.org>
2011-05-20 11:56:56 -0600

    msm: kgsl: Make KGSL components buildable as modules
    Make kgsl-core, adreno and z180 buildable and usable as loadable
    modules.  Export global core symbols for use by devices.
    
    Change-Id: Id2fdd9a1ec53cc4ee9a13ae9e13fedec67e46b9b
    Signed-off-by: Jordan Crouse <jcrouse@codeaurora.org>

--- drivers/gpu/msm/kgsl_pwrctrl.c
+++ drivers/gpu/msm/kgsl_pwrctrl.c
@@ -276,15 +276,6 @@ int kgsl_pwrctrl_init_sysfs(struct kgsl_device *device)
 	return ret;
 }
 
-unsigned long kgsl_get_clkrate(struct clk *clk)
-{
-	if (clk != NULL)  {
-		return clk_get_rate(clk);
-	}  else   {
-		return 0;
-	}
-}
-
 void kgsl_pwrctrl_uninit_sysfs(struct kgsl_device *device)
 {
 	device_remove_file(device->dev, &gpuclk_attr);
@@ -363,6 +354,7 @@ void kgsl_pwrctrl_clk(struct kgsl_device *device, unsigned int pwrflag)
 		return;
 	}
 }
+EXPORT_SYMBOL(kgsl_pwrctrl_clk);
 
 void kgsl_pwrctrl_axi(struct kgsl_device *device, unsigned int pwrflag)
 {
@@ -402,6 +394,7 @@ void kgsl_pwrctrl_axi(struct kgsl_device *device, unsigned int pwrflag)
 		return;
 	}
 }
+EXPORT_SYMBOL(kgsl_pwrctrl_axi);
 
 
 void kgsl_pwrctrl_pwrrail(struct kgsl_device *device, unsigned int pwrflag)
@@ -448,7 +441,7 @@ void kgsl_pwrctrl_pwrrail(struct kgsl_device *device, unsigned int pwrflag)
 		return;
 	}
 }
-
+EXPORT_SYMBOL(kgsl_pwrctrl_pwrrail);
 
 void kgsl_pwrctrl_irq(struct kgsl_device *device, unsigned int pwrflag)
 {
@@ -478,6 +471,7 @@ void kgsl_pwrctrl_irq(struct kgsl_device *device, unsigned int pwrflag)
 		return;
 	}
 }
+EXPORT_SYMBOL(kgsl_pwrctrl_irq);
 
 int kgsl_pwrctrl_init(struct kgsl_device *device)
 {
@@ -670,6 +664,7 @@ void kgsl_pre_hwaccess(struct kgsl_device *device)
 	if (device->state & (KGSL_STATE_SLEEP | KGSL_STATE_NAP))
 		kgsl_pwrctrl_wake(device);
 }
+EXPORT_SYMBOL(kgsl_pre_hwaccess);
 
 void kgsl_check_suspended(struct kgsl_device *device)
 {
@@ -736,7 +731,7 @@ clk_off:
 
 	return 0;
 }
-
+EXPORT_SYMBOL(kgsl_pwrctrl_sleep);
 
 /******************************************************************/
 /* Caller must hold the device mutex. */
@@ -766,4 +761,4 @@ void kgsl_pwrctrl_wake(struct kgsl_device *device)
 	wake_lock(&device->idle_wakelock);
 	KGSL_PWR_INFO(device, "wake return for device %d\n", device->id);
 }
-
+EXPORT_SYMBOL(kgsl_pwrctrl_wake);
