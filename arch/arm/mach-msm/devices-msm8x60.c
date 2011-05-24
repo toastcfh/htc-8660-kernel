60d74402f728af69e3f5b31e5c22aca346f09194
Lucille Sylvester <lsylvest@codeaurora.org>
2011-05-24 16:51:35 -0600

    msm: kgsl: Update BW requests to new clock plan
    The new freq will save several mA active power without
    hurting performance.
    
    Change-Id: I211ee2a873b0ec8d7bd1af08df925b193d02329b
    Signed-off-by: Lucille Sylvester <lsylvest@codeaurora.org>

--- arch/arm/mach-msm/devices-msm8x60.c
+++ arch/arm/mach-msm/devices-msm8x60.c
@@ -566,7 +566,7 @@ static struct msm_bus_vectors grp3d_nominal_low_vectors[] = {
 		.src = MSM_BUS_MASTER_GRAPHICS_3D,
 		.dst = MSM_BUS_SLAVE_EBI_CH0,
 		.ab = 0,
-		.ib = 1397000000U,
+		.ib = 1300000000U,
 	},
 };
 
@@ -628,7 +628,7 @@ static struct msm_bus_vectors grp2d0_max_vectors[] = {
 		.src = MSM_BUS_MASTER_GRAPHICS_2D_CORE0,
 		.dst = MSM_BUS_SLAVE_EBI_CH0,
 		.ab = 0,
-		.ib = 1397000000U,
+		.ib = 1300000000U,
 	},
 };
 
@@ -663,7 +663,7 @@ static struct msm_bus_vectors grp2d1_max_vectors[] = {
 		.src = MSM_BUS_MASTER_GRAPHICS_2D_CORE1,
 		.dst = MSM_BUS_SLAVE_EBI_CH0,
 		.ab = 0,
-		.ib = 1397000000U,
+		.ib = 1300000000U,
 	},
 };
 
