83f146d3cfefa36a446a242311222cb4915184de
Jordan Crouse <jcrouse@codeaurora.org>
2011-05-27 11:05:16 -0600

    msm: kgsl: Rename and expand calc_idle
    Rather then just calculating the idle time and returning it,
    there is value in returning the raw statistics and let the caller
    use them as they see fit.  Rename calc_idle to power_stats, and
    use a structure to pass back the total_time and busy_time.  The
    structure allows for future expansion as well as providing enough
    information for others to perform interesting power and load related
    calculations.
    
    Change-Id: I5132060fd6c4c6867b39a15fccb2380108394225
    Signed-off-by: Jordan Crouse <jcrouse@codeaurora.org>

--- drivers/gpu/msm/kgsl_device.h
+++ drivers/gpu/msm/kgsl_device.h
@@ -67,6 +67,7 @@ struct kgsl_device;
 struct platform_device;
 struct kgsl_device_private;
 struct kgsl_context;
+struct kgsl_power_stats;
 
 struct kgsl_functable {
 	/* Mandatory functions - these functions must be implemented
@@ -104,7 +105,8 @@ struct kgsl_functable {
 		struct kgsl_pagetable *pagetable);
 	int (*cleanup_pt)(struct kgsl_device *device,
 		struct kgsl_pagetable *pagetable);
-	unsigned int (*idle_calc)(struct kgsl_device *device);
+	void (*power_stats)(struct kgsl_device *device,
+		struct kgsl_power_stats *stats);
 	/* Optional functions - these functions are not mandatory.  The
 	   driver will check that the function pointer is not NULL before
 	   calling the hook */
@@ -198,6 +200,11 @@ struct kgsl_process_private {
 struct kgsl_device_private {
 	struct kgsl_device *device;
 	struct kgsl_process_private *process_priv;
+};
+
+struct kgsl_power_stats {
+	s64 total_time;
+	s64 busy_time;
 };
 
 struct kgsl_device *kgsl_get_device(int dev_idx);
