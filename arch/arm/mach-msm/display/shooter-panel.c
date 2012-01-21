2201ab87beeaefc0aeb35861629a0b6d9bbbeced
toastcfh <toastcfh@gmail.com>
2012-01-21 18:23:21 -0500

    ms : shooter: display: add MDP version support
    Adds the MDP version to the respective board file. It will
    be used to differentiate the MDP HW version dependant code
    in MDP & MIPI DSI drivers. Right now it is added only for
    the targets that support MIPI.

--- arch/arm/mach-msm/display/shooter-panel.c
+++ arch/arm/mach-msm/display/shooter-panel.c
@@ -1123,6 +1123,7 @@ static struct msm_panel_common_pdata mdp_pdata = {
 #ifdef CONFIG_MSM_BUS_SCALING
 	.mdp_bus_scale_table = &mdp_bus_scale_pdata,
 #endif
+	.mdp_rev = MDP_REV_41,
 	.mdp_color_enhance = shooter_mdp_color_enhance,
 	.mdp_gamma = shooter_mdp_gamma,
 };
