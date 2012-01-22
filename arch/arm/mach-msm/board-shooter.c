20c2fc5a7ffac98cb7fa9b6740e6379a7e1ef1f8
Praveen Chidambaram <pchidamb@codeaurora.org>
2012-01-21 19:13:34 -0500

    msm: shooter: pm: Remove unused latency and residency values
    Targets that use cpuidle use power calculations to determine the
    sleep mode that the cpu can enter. Older targets without cpuidle
    will still continue to use residency and latency to determine the
    sleep mode to enter.
    
    Change-Id: I874ae4772f8d5cab3f29cda9704bd16e8d8d29e7
    Signed-off-by: Praveen Chidambaram <pchidamb@codeaurora.org>

--- arch/arm/mach-msm/board-shooter.c
+++ arch/arm/mach-msm/board-shooter.c
@@ -353,48 +353,36 @@ static struct msm_pm_platform_data msm_pm_data[MSM_PM_SLEEP_MODE_NR * 2] = {
 		.supported = 1,
 		.suspend_enabled = 1,
 		.idle_enabled = 1,
-		.latency = 4000,
-		.residency = 13000,
 	},
 
 	[MSM_PM_MODE(0, MSM_PM_SLEEP_MODE_POWER_COLLAPSE_STANDALONE)] = {
 		.supported = 1,
 		.suspend_enabled = 1,
 		.idle_enabled = 1,
-		.latency = 500,
-		.residency = 6000,
 	},
 
 	[MSM_PM_MODE(0, MSM_PM_SLEEP_MODE_WAIT_FOR_INTERRUPT)] = {
 		.supported = 1,
 		.suspend_enabled = 1,
 		.idle_enabled = 1,
-		.latency = 2,
-		.residency = 0,
 	},
 
 	[MSM_PM_MODE(1, MSM_PM_SLEEP_MODE_POWER_COLLAPSE)] = {
 		.supported = 1,
 		.suspend_enabled = 1,
 		.idle_enabled = 1,
-		.latency = 600,
-		.residency = 7200,
 	},
 
 	[MSM_PM_MODE(1, MSM_PM_SLEEP_MODE_POWER_COLLAPSE_STANDALONE)] = {
 		.supported = 1,
 		.suspend_enabled = 1,
 		.idle_enabled = 1,
-		.latency = 500,
-		.residency = 6000,
 	},
 
 	[MSM_PM_MODE(1, MSM_PM_SLEEP_MODE_WAIT_FOR_INTERRUPT)] = {
 		.supported = 1,
 		.suspend_enabled = 1,
 		.idle_enabled = 1,
-		.latency = 2,
-		.residency = 0,
 	},
 };
 
