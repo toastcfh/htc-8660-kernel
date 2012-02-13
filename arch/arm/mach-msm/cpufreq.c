3a007a30092940f9e0d15071c7aca36c18ed4c26
Giulio Cervera <giulio.cervera@gmail.com>
2012-02-13 16:33:29 +0100

    msm: cpufreq: Add missing cpufreq_attr

--- arch/arm/mach-msm/cpufreq.c
+++ arch/arm/mach-msm/cpufreq.c
@@ -284,6 +284,11 @@ static ssize_t store_mfreq(struct sysdev_class *class,
 	return count;
 }
 
+static struct freq_attr *msm_cpufreq_attr[] = {
+	&cpufreq_freq_attr_scaling_available_freqs,
+	NULL,
+};
+
 static SYSDEV_CLASS_ATTR(mfreq, 0200, NULL, store_mfreq);
 
 static struct cpufreq_driver msm_cpufreq_driver = {
@@ -293,6 +298,7 @@ static struct cpufreq_driver msm_cpufreq_driver = {
 	.verify		= msm_cpufreq_verify,
 	.target		= msm_cpufreq_target,
 	.name		= "msm",
+	.attr		= msm_cpufreq_attr,
 };
 
 static struct notifier_block msm_cpufreq_pm_notifier = {
